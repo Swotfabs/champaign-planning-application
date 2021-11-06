70f69b6667e1919a34aca262fdc0fb9d35ab31b3
Fabian Junge <fabsjunge@gmail.com>
2021-11-05 22:32:11 -0500

    Selection is now a dropdown menu controlled by React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
+import React from 'react';
 import './App.css';
 
-// TODO replace with dropdown selector
-function Dropdown(props) {
-  return (
-    <p>
-        Year will go here.
-    </p>
-  );
+class YearSelect extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console.log('Year changed to ' + event.target.value);
+    this.setState({value: event.target.value});
+  }
+
+  // handleSubmit will call a function passed in by props
+  handleSubmit(event) {
+    console.log('A year was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render () {
+    // Years will be an attribute of props but is defined here for now.
+    const years = ['2010', '2011', '2012']
+
+    let yearsOptions = []
+    years.forEach((year) => {
+      const option = (
+        <option key={year} value={year}>{year}</option>
+        );
+      yearsOptions = yearsOptions.concat([option]);
+    });
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+        Select a year: 
+          <select name="years" id="years" value={this.state.value} onChange={this.handleChange}>
+            {yearsOptions}
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 // TODO replace with Bar Graph
@@ -27,7 +65,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Dropdown />
+        <YearSelect />
         <RenderData />
       </header>
     </div>
